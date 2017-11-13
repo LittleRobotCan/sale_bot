af58420be554326dd2bc8e3e1234f5fffd91dfb5
RuiweiJiang <ruiwei06@gmail.com>
2017-11-13 15:24:10 -0800

    setup run time configs

--- run_bot.py
+++ run_bot.py
@@ -1,3 +1,5 @@
-from .artizia import *
+from retailers.aritzia import *
 
-print get_sale_items('sweate')
+sale_sweaters = get_sale_items('sweater')
+for item, url in sale_sweaters.iteritems():
+    print item, url
